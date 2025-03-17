9072e0a81683817bc307a1cc33dd353740715b60
GleisonMelo <gleisonmelo1d2015@gmail.com>
2025-03-17 23:06:22 +0000

    Criando as variaveis do super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,13 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    char estado; //Variavel de estado
+    char codigo[50]; //Variavel de codigo do estado
+    char cidade[50]; //variavel do nome da cidade
+    int populacao; //variavel da população
+    float area; //variavel da area por km
+    float PIB; //variavel do pib 
+    int NPT; //variavel do numero de pontos turisticos da cidade
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
     return 0;
 }
