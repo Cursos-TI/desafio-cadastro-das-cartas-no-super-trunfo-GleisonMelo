65af97f441ab3fb178634e188573da1f648243e7
GleisonMelo <gleisonmelo1d2015@gmail.com>
2025-03-18 00:25:53 +0000

    Projeto acabado, pedindo, salvando e mostrando as 2 cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,6 +19,16 @@
         int NPT; //variavel do numero de pontos turisticos da cidade
     
     }Carta;
+    void mostrarCarta(Carta carta){
+        printf("Estado: %s \n", carta.estado);
+        printf("Codigo da carta: %s \n", carta.codigo);
+        printf("Cidade: %s \n", carta.cidade);
+        printf("População: %d \n", carta.populacao);
+        printf("Area: %.2f \n", carta.area);
+        printf("PIB: %.2f \n", carta.PIB);
+        printf("Numero de pontos turisticos: %d \n", carta.NPT);
+
+    }
 int main() {
     Carta carta1, carta2;
     
@@ -33,37 +43,37 @@ int main() {
     printf("Digite a cidade:\n"); // pedindo o nome da cidade
     scanf("%s", carta1.cidade);// salvado o nome
     printf("Digite a quantidade de população da cidade: \n");//pedindo a população
-    scanf("%d",carta1.populacao);// salvando a população
+    scanf("%d", &carta1.populacao); // salvando a população
     printf("Digite a area por KM²: \n"); //pedindo a area do local
-    scanf("%f", carta1.area); // salvando a area
+    scanf("%f", &carta1.area); // salvando a area
     printf("Digite o PIB da cidade: \n"); //Pedindo o PIB   
-    scanf("%f", carta1.PIB); //salvando o PIB
+    scanf("%f", &carta1.PIB); //salvando o PIB
     printf("Digite o numero de pontos turisticos da cidade: \n"); // pedindo a quantidade dos pontos
-    scanf("%d", carta1.NPT); // Salvando os pontos
+    scanf("%d", &carta1.NPT); // Salvando os pontos
 
     printf("Crie a segunda carta.\n"); // primeira carta
 
-    printf("Digite o estado da primeira carta de A a H: \n"); // pedindo o estado da primeira carta
+    printf("Digite o estado da segunda carta de A a H: \n"); // pedindo o estado da primeira carta
     scanf("%s", carta2.estado); // salvado o estado
     printf("Digite o codigo do estado seguido da letra entre 01 e 04:\n"); // pedindo o codigo do estado
     scanf("%s", carta2.codigo); // salvando o codigo
     printf("Digite a cidade:\n"); // pedindo o nome da cidade
     scanf("%s", carta2.cidade);// salvado o nome
     printf("Digite a quantidade de população da cidade: \n");//pedindo a população
-    scanf("%d",carta2.populacao);// salvando a população
+    scanf("%d", &carta2.populacao);// salvando a população
     printf("Digite a area por KM²: \n"); //pedindo a area do local
-    scanf("%f", carta2.area); // salvando a area
+    scanf("%.f", &carta2.area); // salvando a area
     printf("Digite o PIB da cidade: \n"); //Pedindo o PIB   
-    scanf("%f", carta2.PIB); //salvando o PIB
+    scanf("%.f", &carta2.PIB); //salvando o PIB
     printf("Digite o numero de pontos turisticos da cidade: \n"); // pedindo a quantidade dos pontos
-    scanf("%d", carta2.NPT); // Salvando os pontos
+    scanf("%d", &carta2.NPT); // Salvando os pontos
 
 
     printf("\ncarta1: \n");
-    exibirCarta(carta1);
+    mostrarCarta(carta1);
 
     printf("\ncarta2: \n");
-    exibirCarta(carta2);
+    mostrarCarta(carta2);
 
     return 0;
 }
